2cfd85194689190de6c9e73854b3e69020ee5641
Tally <tallyl@traiana.com>
2021-12-06 22:38:36 +0200

    execersing

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -4,3 +4,6 @@ sudo sh -c ' echo  "192.168.100.11    server2" >> /etc/hosts'
 sudo sh -c ' echo  "192.168.100.10    server1" >> /etc/hosts'
 
 #sudo vi /etc/ssh/sshd_config
+# Allowing root login
+sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+sudo echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
--- materials/vagrant/fixScripts/exercise4-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server2.sh
@@ -2,3 +2,7 @@
 #add fix to exercise4-fix here
 sudo sh -c ' echo  "192.168.100.11    server2" >> /etc/hosts'
 sudo sh -c ' echo  "192.168.100.10    server1" >> /etc/hosts'
+
+# Allowing root login
+sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+sudo echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
