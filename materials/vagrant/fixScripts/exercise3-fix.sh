c30021f7c9eab8e4141c82d943df7e7500f11a88
Tally <tallyl@traiana.com>
2021-12-06 13:32:37 +0200

    execersing

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 #add fix to exercise3 here
+sudo apt-get update
+sudo apt-get install -y apache2
+if ! [ -L /var/www ]; then
+  sudo rm -rf /var/www
+  sudo ln -fs /vagrant /var/www
+fi
+
