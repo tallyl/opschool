5ceb0fa918be20bf4a3ee0a03ab66ad7f5d5834e
Tally <tallyl@traiana.com>
2021-12-06 13:47:09 +0200

    execersing

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #add fix to exercise3 here
-sudo apt-get update
-sudo apt-get install -y apache2
-if ! [ -L /var/www ]; then
-  sudo rm -rf /var/www
-  sudo ln -fs /vagrant /var/www
-fi
+#sudo apt-get update
+#sudo apt-get install -y apache2
+#sudo mkdir -p /vagrant/html
+#if ! [ -L /var/www ]; then
+#  sudo rm -rf /var/www
+#  sudo ln -fs /vagrant /var/www
+#fi
+
+#sudo  systemctl start apache2
 
