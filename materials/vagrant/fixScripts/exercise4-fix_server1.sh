fa7e65c952fc1bb9d9259ecafa8d1d6d6662119f
Tally <tallyl@traiana.com>
2021-12-06 18:34:49 +0200

    execersing

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-#add fix to exercise6-fix here
+#add fix to exercise4-fix here
+sudo sh -c ' echo  "192.168.100.11    server2" >> /etc/hosts'
+sudo sh -c ' echo  "192.168.100.10    server1" >> /etc/hosts'
