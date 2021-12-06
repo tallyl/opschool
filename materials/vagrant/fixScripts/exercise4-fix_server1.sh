ed73c5149b4a189776323317f752fbe3bf203886
Tally <tallyl@traiana.com>
2021-12-06 22:04:20 +0200

    execersing

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -2,3 +2,5 @@
 #add fix to exercise4-fix here
 sudo sh -c ' echo  "192.168.100.11    server2" >> /etc/hosts'
 sudo sh -c ' echo  "192.168.100.10    server1" >> /etc/hosts'
+
+#sudo vi /etc/ssh/sshd_config
