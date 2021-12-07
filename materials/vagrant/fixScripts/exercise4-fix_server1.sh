31b14535b7ef4e2dc91eb3075b92e75c66faed11
Tally <tallyl@traiana.com>
2021-12-07 12:36:57 +0200

    almost done

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -7,3 +7,4 @@ sudo sh -c ' echo  "192.168.100.10    server1" >> /etc/hosts'
 # Allowing root login
 sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 sudo echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+sudo service sshd restart
