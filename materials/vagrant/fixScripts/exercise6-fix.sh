a59a0fb22f9118e13d0896a620378981a1b721aa
Tally <tallyl@traiana.com>
2021-12-07 15:20:01 +0200

    execersing

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+
 external_param=$#
 if [ $external_param -lt 2 ] ; then
    echo "Proper usage: exercise6-fix.sh must recive at list 2 arguments "
    echo " Usage : exercise6-fix.sh  [ file list ] path_to_copy"
    exit 1
 fi
+
 
 ARGS=("$@")
 # Get the last argument
@@ -20,29 +22,33 @@ host=`hostname`
 #echo $host
 host_id=${host: -1}
 
-echo "host id " $host_id
+#echo "host id " $host_id
 
 if [ $host_id == 1 ]; then
-   echo " We copy to server2"
+   dest_server=${host%?}2
+#   echo " We copy to " $dest_server
 elif [ $host_id == 2 ]; then
-   echo " We copy to server1"
+   dest_server=${host%?}1
+#   echo " We copy to " $dest_server
 else :
     echo "This only work on servers called : server1 / server2"
     exit 1
 fi
 
-echo "Path is : " $path 
+#echo "Path is : " $path 
 
 for var in "${ARGS[@]}"
 do
     if [ -f $var ]; then
            FILESIZE=$(stat -c%s "$var")
-           echo " size of file "  $var " is" $FILESIZE " bytes"
+           #echo " size of file "  $var " is" $FILESIZE " bytes"
            sum=$(($sum + $FILESIZE))
+           echo "scp " $var " "  $dest_server":"$path"/" 
+		   scp $var $dest_server:$path/
     else
        echo "file" $var " does not exist !! "
     fi
 done
 
-echo " Sum of byes " $sum
 
+echo $sum
